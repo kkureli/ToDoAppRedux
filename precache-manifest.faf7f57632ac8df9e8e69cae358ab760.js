14d3ca25d5aaeac0921111306c04ad60a9d50c0f
Kaan <kkureli@gmail.com>
2020-03-29 14:26:05 +0300

    Updates

---
+++ precache-manifest.faf7f57632ac8df9e8e69cae358ab760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c51ca90a67d18739a8d9caebb1ac82",
+    "url": "/ToDoAppRedux/index.html"
+  },
+  {
+    "revision": "7bce0fb436554a9ba182",
+    "url": "/ToDoAppRedux/static/css/main.46ce3400.chunk.css"
+  },
+  {
+    "revision": "a4bd5c4a9025821f7c27",
+    "url": "/ToDoAppRedux/static/js/2.643ca3c4.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ToDoAppRedux/static/js/2.643ca3c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bce0fb436554a9ba182",
+    "url": "/ToDoAppRedux/static/js/main.a6cf19f3.chunk.js"
+  },
+  {
+    "revision": "fa47a567b011cad4df33",
+    "url": "/ToDoAppRedux/static/js/runtime-main.5eca990c.js"
+  }
+]);
